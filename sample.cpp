be644f74c324f13647124e04e8e895fe45af68eb
kalyan1412 <47789050+kalyan1412@users.noreply.github.com>
2019-12-27 12:52:37 +0530

    Create sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+class Sample
+{
+  private:
+  int height,width;
+  public:
+  void set_height(int);
+  void set_width(int);
+  int get_height();
+  int get_width();
+};
+
+void Sample :: set_height(int height)
+{
+  this->height=height;
+}
+
+void Sample :: set_width(int width)
+{
+  this->width=width;
+}
+
+int Sample :: get_height()
+{
+  return height;
+}
+
+int Sample :: get_width()
+{
+  return width;
+}
+
+int main()
+{
+  Sample s;
+  s.set_height(10);
+  s.set_width(4);
+  cout<<"Height="<<s.get_height()<<endl;
+  cout<<"Width="<<s.get_width()<<endl;
+  return 0;
+  }
+  
